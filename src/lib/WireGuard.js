de08c8e40d9f1b48d14faa6373690ab6e733a154
jcosmao <j.cosmao@gmail.com>
2024-04-14 12:51:20 +0200

    Update allowed Ips from xwvike/allowed-ips
      - Fix dark mode
      - adjust style to fit wg-easy theme
      - added check for valid ipv4
      - added js helper to jump on next input when '.' pressed
      - rm allowed ip from New client create for simplicity

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -111,7 +111,7 @@ PostDown = ${WG_POST_DOWN}
         }
         allowedIPs = allowedIPs.substring(1);
       } else {
-        allowedIPs = client.address;
+        allowedIPs = `${client.address}/32`;
       }
 
       result += `
@@ -229,7 +229,7 @@ Endpoint = ${WG_HOST}:${WG_PORT}`;
     });
   }
 
-  async createClient({ name, allowedIps }) {
+  async createClient({ name }) {
     if (!name) {
       throw new Error('Missing: Name');
     }
@@ -240,20 +240,16 @@ Endpoint = ${WG_HOST}:${WG_PORT}`;
     const publicKey = await Util.exec(`echo ${privateKey} | wg pubkey`);
     const preSharedKey = await Util.exec('wg genpsk');
 
+    // Calculate next IP
     let address;
-    if (allowedIps) {
-      address = allowedIps;
-    } else {
-      // Calculate next IP
-      for (let i = 2; i < 255; i++) {
-        const client = Object.values(config.clients).find((client) => {
-          return client.address.includes(WG_DEFAULT_ADDRESS.replace('x', i));
-        });
+    for (let i = 2; i < 255; i++) {
+      const client = Object.values(config.clients).find((client) => {
+        return client.address.includes(WG_DEFAULT_ADDRESS.replace('x', i));
+      });
 
-        if (!client) {
-          address = `${WG_DEFAULT_ADDRESS.replace('x', i)}`;
-          break;
-        }
+      if (!client) {
+        address = `${WG_DEFAULT_ADDRESS.replace('x', i)}`;
+        break;
       }
     }
 
